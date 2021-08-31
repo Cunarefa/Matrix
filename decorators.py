94ddb02c89f712f156bea9fddc39b9f57dc11b07
Yevgen <yevgen.yelik@gmail.com>
2021-08-31 12:54:48 +0300

    "Two decorators, add zero checking"

--- decorators.py
+++ decorators.py
@@ -1,15 +1,22 @@
 from functools import wraps
 
 
-def check_matching(fun):
+def check_summ_conditions(fun):
     @wraps(fun)
     def decorated(*args, **kwargs):
-        if args[0].shape[1] == args[1].shape[0]:
-            return fun(*args, **kwargs)
-        elif args[0].shape[0] == args[1].shape[0] and args[0].shape[1] == args[1].shape[1] and args[0].shape[1] != \
-                args[1].shape[0]:
+        if args[0].shape[0] == args[1].shape[0] and args[0].shape[1] == args[1].shape[1]:
             return fun(*args, **kwargs)
         else:
             raise ValueError("Wrong length of elements list")
 
     return decorated
+
+
+def check_mult_conditions(fun):
+    @wraps(fun)
+    def wrapped(*args, **kwargs):
+        if args[0].shape[1] == args[1].shape[0]:
+            return fun(*args, **kwargs)
+        raise ValueError("Wrong length of elements list")
+
+    return wrapped
--- matrix.py
+++ matrix.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from decorators import check_matching
+from decorators import check_summ_conditions, check_mult_conditions
 
 
 class Matrix():
@@ -14,12 +14,15 @@ class Matrix():
 
 class Operations():
     @staticmethod
-    @check_matching
+    @check_mult_conditions
     def mult(m1, m2):
-        return m1.dot(m2)
+        sums_of_elem = [sum(elem) for elem in m2.tolist()]
+        if any(sums_of_elem) > 0:
+            return m1.dot(m2)
+        return m2
 
     @staticmethod
-    @check_matching
+    @check_summ_conditions
     def summ(m1, m2):
         return m1 + m2
 
@@ -28,7 +31,11 @@ m1 = Matrix()
 m2 = Matrix()
 o = Operations()
 
-first_m = m1.numpy_data([[1, 2, 3], [4, 5, 6]])
-second_m = m2.numpy_data([[1, 2, 3], [0, 8, 9], [12, 45, 2]])
+list_a = [[1, 2, 3], [4, 5, 6]]
+list_b = [[1, 2, 3], [0, 8, 9], [12, 45, 2]]
+zero_list = np.full(6, 0).reshape(2, 3)
+
+first_m = m1.numpy_data(list_a)
+second_m = m2.numpy_data(list_b)
 
 print(o.mult(first_m, second_m))
